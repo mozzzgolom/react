537beceb2836997aa3681a26bc3f740e433e8e83
mozzzgolom <mozzzgolom@gmal.com>
2021-08-13 12:06:54 +0300

    add chats

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -1,12 +1,26 @@
 // @ts-ignore
-import styles from "./App.module.sass";
-//import { Message } from "./component/Message";
+import { Grid, makeStyles } from "@material-ui/core"
+import { Chats } from "./component/Chats"
 import { MessageChat } from "./component/MessageChat/MessageChat.js"
 
+const useStyles = makeStyles({
+  app: {
+    fontFamily: "Roboto, Helvetica, sans-serif",
+  },
+})
+
 export function App() {
+  const classes = useStyles()
   return (
-    <div className={styles.app}>
-      <MessageChat />
+    <div className={classes.app}>
+      <Grid container={true}>
+        <Grid item={true} xs={12} md={3}>
+          <Chats />
+        </Grid>
+        <Grid item={true} xs={12} md={9}>
+          <MessageChat />
+        </Grid>
+      </Grid>
     </div>
   )
 }
